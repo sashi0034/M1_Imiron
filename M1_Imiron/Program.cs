5e906845288858d36649cb02cf0529d6129adf98
sashi <satoshi3104.nishi@gmail.com>
2025-04-25 08:50:39 +0900

    Test プロジェクト

--- M1_Imiron/Program.cs
+++ M1_Imiron/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    public static void Main()
+    {
+        // See https://aka.ms/new-console-template for more information
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Xunit;
+
+namespace Test;
+
+public class Test
+{
+    [Fact]
+    public void This_is_a_test()
+    {
+        Assert.Equal(2 + 3, 5);
+    }
+}
