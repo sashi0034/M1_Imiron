00a2706b731aec46348103611df075359468cb14
sashi <satoshi3104.nishi@gmail.com>
2025-04-25 08:40:53 +0900

    プロジェクト ファイルを追加します。

---
+++ M1_Imiron/Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace M1_Imiron
+{
+    public class Test
+    {
+        [Fact]
+        public void Add_AddsTwoNumbers_ReturnsCorrectSum()
+        {
+            Assert.Equal(7, 3 + 4);
+        }
+
+        [Fact]
+        public void Subtract_SubtractsTwoNumbers_ReturnsCorrectDifference()
+        {
+            Assert.Equal(6, 3 + 3);
+        }
+    }
+}
