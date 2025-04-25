681a235fdc55c3f14f847998d169699802bf94e2
sashi <satoshi3104.nishi@gmail.com>
2025-04-25 11:55:02 +0900

    テスト追加

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,14 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using M1_Imiron;
 using Xunit;
 
 namespace Test;
 
-public class Test
+public static class Test
 {
-    [Fact]
-    public void This_is_a_test()
+    private static void assertEqual(string expected, string actual)
     {
-        Assert.Equal(2 + 3, 5);
+        var expectedLines = expected.Split('\n');
+        var actualLines = actual.Split('\n');
+
+        Assert.Equal(expectedLines.Length, actualLines.Length);
+
+        for (int i = 0; i < expectedLines.Length; i++)
+        {
+            Assert.Equal(expectedLines[i].Trim(), actualLines[i].Trim());
+        }
+    }
+
+
+    [Fact]
+    public static void Example_0()
+    {
+        assertEqual(
+            """
+            S(Z) times S(Z) is S(Z) by T-Succ {
+               Z times S(Z) is Z by T-Zero {};
+               S(Z) plus Z is S(Z) by P-Succ {
+                   Z plus Z is Z by P-Zero {}
+               }
+            }
+            """, Reduction.Reduce("S(Z) times S(Z) is S(Z)"));
     }
 }
